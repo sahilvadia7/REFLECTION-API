73aab05a9265e19f28fc964177e91534589a4486
sahilvadia <vadiasahil23@example.com>
2025-07-17 16:59:48 +0530

    custom annotation

--- src/main/java/org/reflection/runner/advanced/annotation/Component.java
+++ src/main/java/org/reflection/runner/advanced/annotation/Component.java
@@ -1,4 +1,14 @@
 package org.reflection.runner.advanced.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface Component {
+    String value() default "";
 }
+
+
--- src/main/java/org/reflection/runner/advanced/annotation/Service.java
+++ src/main/java/org/reflection/runner/advanced/annotation/Service.java
@@ -1,4 +1,13 @@
 package org.reflection.runner.advanced.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Component
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface Service {
+    String value() default "";
 }
